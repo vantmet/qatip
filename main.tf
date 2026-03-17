873dff3a61a664ced2a808798ae6422354e78eac
Anthony Metcalf <anthony.metcalf1@gmail.com>
2026-03-17 15:36:06 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.16.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "<your subscription id here>"
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "RG1"
+    storage_account_name = "<your storage account>"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "RG2"
+  location = "westeurope"
+}
