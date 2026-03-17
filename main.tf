9aac39064de6a8633d2ab6fdd00f7c5095b5809b
Anthony Metcalf <anthony.metcalf1@gmail.com>
2026-03-17 15:40:31 +0000

    Update Azure subscription and storage account details

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "<your subscription id here>"
+  subscription_id = "689ec7d5-82d1-4115-9716-461253177cbc"
 }
 
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    storage_account_name = "jenkinsstatevantmet"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
